3dcd4dd18218158e44a59e05c48eddec0d4e5c95
hello <1204611652@qq.com>
2024-07-05 15:31:52 +0800

    1.0.0

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+        <h1>123</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // 开启ssr渲染 默认开启
+  ssr:true,
   devtools: { enabled: true }
 })
